6b6db9762585b72718d5cd00073a2ce1e0ded651
Nivrutti pawar <159421324+Niruttipawar@users.noreply.github.com>
2026-08-03 17:02:07 +0530

    Delete tuplle copy.py

--- tuplle copy.py
+++
@@ -1,6 +0,0 @@
-a=(1,2,2,2.2,3.3)
-
-'''index=a.index(2.2) ''' #this is an use to find the index of a number by using the index method
-count=a.count(2)  #this is an use to find the count of a number by using the count method
-
-print(count)
